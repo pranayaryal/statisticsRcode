03797b1917f17dea741b24499808294797282a87
Pranay Aryal <drpranayaryal@gmail.com>
2015-01-28 00:00:47 -0500

    Update anova.R

--- anova.R
+++ anova.R
@@ -11,8 +11,24 @@ boxplot(score~method,data=anov)
 ##one-way analysis of means
 oneway.test(score~method,data=anov,var.equal=T)
 
+##looking at variances of group
+with(anov,tapply(score,method,var))
+
 ##levene test to look for homogeniety of variance
 levene.test(anov$score,anov$method,
             location="median")
 
-##we will continue
+##checking normality in residuals.Create an object to store the aov
+obj<-aov(iop~formulation,data=iop)
+## the class of this object  class(obj) will give "lm" "aov"
+
+##this will give you the value of residuals(a vector of integers)
+obj$residuals
+
+##this can be added to the dataframe as a separate variable
+anov$residuals<-obj$residuals
+
+## checking normality
+shapiro.test(anov$residuals)
+
+
